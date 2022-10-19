7271750d7c5d9d33dabe388580169e424db5de46
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2022-10-19 00:07:15 -0300

    splash screen, fix app icons and most listened screen padding

--- app/src/main/java/io/musicorum/mobile/components/BottomNavbar.kt
+++ app/src/main/java/io/musicorum/mobile/components/BottomNavbar.kt
@@ -2,14 +2,17 @@ package io.musicorum.mobile.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import io.musicorum.mobile.ui.theme.KindaBlack
+import io.musicorum.mobile.ui.theme.MostlyRed
 import java.util.*
 
 @Composable
@@ -22,18 +25,27 @@ fun BottomNavBar(current: String, nav: NavHostController) {
         Icons.Rounded.BarChart,
         Icons.Rounded.Person
     )
+    val navItemColors = NavigationBarItemDefaults.colors(
+        indicatorColor = MostlyRed,
+        selectedIconColor = Color.White,
+        selectedTextColor = Color.White
+    )
 
-    Box(modifier = Modifier.background(KindaBlack)) {
+    Box(
+        modifier = Modifier
+            .background(KindaBlack)
+    ) {
         NavigationBar(
             containerColor = Color.Transparent
         ) {
             items.forEachIndexed { index, s ->
                 NavigationBarItem(
                     selected = s == current,
-                    label = { Text(text = s) },
+                    label = { Text(text = s, modifier = Modifier.padding(top = 60.dp)) },
                     onClick = { nav.navigate(s.lowercase(Locale.ROOT)) },
                     icon = { Icon(icons[index], contentDescription = "nav icon") },
                     alwaysShowLabel = false,
+                    colors = navItemColors
                 )
 
             }
