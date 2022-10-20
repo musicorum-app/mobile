4b6cfd44aa7e7c4bdfe6a838f2c56307cba980b8
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2022-10-20 14:40:55 -0300

    similar track work in progress - do not build

--- app/src/main/java/io/musicorum/mobile/viewmodels/TrackViewModel.kt
+++ app/src/main/java/io/musicorum/mobile/viewmodels/TrackViewModel.kt
@@ -3,12 +3,15 @@ package io.musicorum.mobile.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.musicorum.mobile.ktor.endpoints.SimilarTracksEndpoint
 import io.musicorum.mobile.ktor.endpoints.TrackEndpoint
+import io.musicorum.mobile.serialization.SimilarTrack
 import io.musicorum.mobile.serialization.Track
 import kotlinx.coroutines.launch
 
 class TrackViewModel : ViewModel() {
     val track: MutableLiveData<Track> by lazy { MutableLiveData<Track>() }
+    val similar: MutableLiveData<SimilarTrack> by lazy { MutableLiveData<SimilarTrack>() }
 
     suspend fun fetchTrack(
         trackName: String,
@@ -19,6 +22,13 @@ class TrackViewModel : ViewModel() {
         viewModelScope.launch {
             val res = TrackEndpoint().getTrack(trackName, artist, username, autoCorrect)
             track.value = res.track
+        }
+    }
+
+    suspend fun fetchSimilar(baseTrack: Track, limit: Int?, autoCorrect: Boolean?) {
+        viewModelScope.launch {
+            val res = SimilarTracksEndpoint().fetchSimilar(baseTrack, limit, autoCorrect)
+            similar.value = res
         }
     }
 }
