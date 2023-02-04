9869af160a692ae7838271dee10f8c765dd661ec
Renan 츄 <lyricalsoul@proton.me>
2023-02-04 19:49:27 +0000

    fix missing envs

--- .space.kts
+++ .space.kts
@@ -9,6 +9,10 @@ job("Build and release to internal testing") {
         env["KEY_STORE_PASSWORD"] = Secrets("key_store_password")
         env["KEY_PASSWORD"] = Secrets("key_password")
         env["KEY_ALIAS"] = Params("key_alias")
+        env["BUILD_CONFIG_PROPERTIES"] = Param("build_config_properties")
+        env["GOOGLE_SERVICES_JSON"] = Param("google_services_json")
+        env["CROWDIN_PROPERTIES"] = Param("crowdin_properties")
+        
 
         shellScript {
             content = """
