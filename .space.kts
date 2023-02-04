1de77363d30372440177be5bf58c49c467784ff8
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2023-02-04 15:52:59 -0300

    jetbrains spaces job configuration

---
+++ .space.kts
@@ -0,0 +1,34 @@
+job("Build and release to internal testing") {
+    startOn {
+        gitPush { enabled = true }
+    }
+
+    container("Build and publish to internal", "musicorum.registry.jetbrains.space/p/main/containers/android-publisher:latest") {
+        env["GOOGLE_SA_KEY"] = Secrets("google_sa_key")
+        env["KEY_STORE"] = Secrets("key_store")
+        env["KEY_STORE_PASSWORD"] = Secrets("key_store_password")
+        env["KEY_PASSWORD"] = Secrets("key_password")
+        env["KEY_ALIAS"] = Params("key_alias")
+
+        shellScript {
+            content = """
+                echo Get private signing key...
+                echo ${'$'}KEY_STORE > upload_key.hex
+                xxd -plain -revert upload_key.hex  upload_key.jks
+                echo Get Google service account key...
+                echo ${'$'}GOOGLE_SA_KEY > google_sa_key.hex
+                xxd -plain -revert google_sa_key.hex  google_sa_key.json
+                echo Get tokens.release.properties...
+                echo ${'$'}BUILD_CONFIG_PROPERTIES > tokens.release.hex
+                xxd -plain -revert build_config_properties.hex  tokens.release.properties
+                echo Get google-services.json...
+                echo ${'$'}GOOGLE_SERVICES_JSON > google_services.hex
+                xxd -plain -revert google_services.hex google-services.json
+                echo "Moving file..."
+                mv ../google-services.json ../app/google-services.json
+                echo Build and pulbish AAB...
+                ./gradlew publishBundle
+            """
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM gradle:6.8.3-jre15
+USER root
+ENV TOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip" \
+ANDROID_SDK_ROOT="/usr/local/android-sdk" \
+ANDROID_SDK=30 \
+ANDROID_BUILD_TOOLS=30.0.3 \
+PATH="${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin:${ANDROID_SDK_ROOT}/cmdline-tools/tools/bin:${PATH}"
+
+# Download Android SDK
+RUN mkdir "$ANDROID_SDK_ROOT" .android "$ANDROID_SDK_ROOT/cmdline-tools" && \
+    cd "$ANDROID_SDK_ROOT/cmdline-tools" && \
+    curl -o sdk.zip $TOOLS_URL && \
+    unzip sdk.zip && \
+    rm sdk.zip && \
+    mv cmdline-tools tools && \
+    yes | $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --licenses
+
+# Install Android Build Tools
+RUN $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager --update
+RUN $ANDROID_SDK_ROOT/cmdline-tools/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" \
+"platforms;android-${ANDROID_SDK}" \
+"platform-tools"
+
+# Install xxd
+RUN apt-get update && \
+    apt-get install xxd
