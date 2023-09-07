1d9bb2cfa596ca2a2e8d80ca9fe9b5f5d52fa1c1
Matheus Sanches <methidezeeni@gmail.com>
2023-09-07 20:46:57 -0300

    feat: re-enable sentry

--- app/src/main/java/io/musicorum/mobile/ktor/KtorConfiguration.kt
+++ app/src/main/java/io/musicorum/mobile/ktor/KtorConfiguration.kt
@@ -2,8 +2,10 @@ package io.musicorum.mobile.ktor
 
 import android.util.Log
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
@@ -14,6 +16,7 @@ import io.ktor.client.plugins.plugin
 import io.ktor.client.request.header
 import io.ktor.client.request.headers
 import io.ktor.client.request.parameter
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.URLProtocol
 import io.ktor.http.isSuccess
 import io.ktor.http.path
@@ -21,6 +24,7 @@ import io.ktor.serialization.kotlinx.json.json
 import io.musicorum.mobile.BuildConfig
 import io.musicorum.mobile.utils.CustomCacheControl
 import io.musicorum.mobile.utils.md5Hash
+import io.sentry.Sentry
 import kotlinx.serialization.json.Json
 
 object KtorConfiguration {
@@ -41,9 +45,9 @@ object KtorConfiguration {
                 validateResponse { res ->
                     if (!res.status.isSuccess()) {
                         if (res.status.value >= 500) {
-                            //Sentry.captureException(ServerResponseException(res, res.bodyAsText()))
+                            Sentry.captureException(ServerResponseException(res, res.bodyAsText()))
                         } else {
-                            //Sentry.captureException(ClientRequestException(res, res.bodyAsText()))
+                            Sentry.captureException(ClientRequestException(res, res.bodyAsText()))
                         }
                     }
                 }
@@ -98,9 +102,9 @@ object KtorConfiguration {
             validateResponse { res ->
                 if (!res.status.isSuccess()) {
                     if (res.status.value >= 500) {
-                        //Sentry.captureException(ServerResponseException(res, res.bodyAsText()))
+                        Sentry.captureException(ServerResponseException(res, res.bodyAsText()))
                     } else {
-                        //Sentry.captureException(ClientRequestException(res, res.bodyAsText()))
+                        Sentry.captureException(ClientRequestException(res, res.bodyAsText()))
                     }
                 }
             }
