5da9115cfac980163fd88c733d338a259e63d2f6
MysteryMS <piva.pedro@outlook.com>
2023-09-23 12:10:15 -0300

    fix track card text padding and scrobbling

--- app/src/main/java/io/musicorum/mobile/components/TrackListItem.kt
+++ app/src/main/java/io/musicorum/mobile/components/TrackListItem.kt
@@ -21,9 +21,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -74,6 +74,7 @@ fun TrackListItem(
                         modifier = Modifier
                             .size(22.dp)
                             .padding(end = 5.dp)
+                            .alpha(.70f)
                     )
                 }
                 Text(
